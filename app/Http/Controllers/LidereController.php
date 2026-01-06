cb69f19b367e6191e79ee451486b0375413bbd38
Andres velez <antares2881@gmail.com>
2026-01-06 17:24:43 -0500

    correccion de funcion getSublideres, se elimina la union de las consultas

--- app/Http/Controllers/LidereController.php
+++ app/Http/Controllers/LidereController.php
@@ -124,31 +124,11 @@ class LidereController extends Controller
         $sublideres = DB::select("SELECT l.id as lidere_id, l.nombres as nombres_lider, l.apellidos as apellidos_lider, sl.id, sl.candidato_id, sl.nombres, sl.apellidos, sl.fecha_nac, sl.meta_votantes, sl.telefono, sl.direccion, sl.profesione_id, (SELECT COUNT(*) FROM listadovotantes as lv WHERE lv.sublidere_id = sl.id) as total_militantes
             FROM sublideres as sl 
             INNER JOIN lideres as l ON sl.lidere_id = l.id
-            WHERE l.id = $id AND l.candidato_id = $candidato");
-
-            // Marcar cada sublider como no-líder
-            if(!empty($sublideres)){
-                foreach($sublideres as $k => $item){
-                    $sublideres[$k]->is_lider = false;
-                }
-            }
+            WHERE l.id = $id AND l.candidato_id = $candidato");            
 
         $lider = DB::select("SELECT l.id as lidere_id, l.nombres as nombres_lider, l.apellidos as apellidos_lider, l.id, l.candidato_id, l.nombres, l.apellidos, l.fecha_nac, l.meta_votantes, l.telefono, l.direccion, l.profesione_id, (SELECT COUNT(*) FROM listadovotantes as lv WHERE lv.lidere_id = l.id AND lv.sublidere_id = 0) as total_militantes
             FROM lideres as l 
             WHERE l.id = $id AND l.candidato_id = $candidato");
-
-            // Marcar el registro del líder
-            if(!empty($lider)){
-                foreach($lider as $k => $item){
-                    $lider[$k]->is_lider = true;
-                }
-            }
-
-        if(!empty($sublideres) && !empty($lider)){
-            $sublideres = array_merge($sublideres, $lider);
-        } elseif(empty($sublideres) && !empty($lider)){
-            $sublideres = $lider;
-        }
 
         $data = array(
             'code' => 200,
